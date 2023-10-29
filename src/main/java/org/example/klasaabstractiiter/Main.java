8bf549eee94cae80e4359d6d03f377d2ec09c4d3
Milena Bilewska <bilewskamilena@gmail.com>
2023-10-29 10:57:17 +0100

    klasa absrakcyjna

--- src/main/java/org/example/klasaabstractiiter/Main.java
+++ src/main/java/org/example/klasaabstractiiter/Main.java
@@ -8,31 +8,31 @@ public class Main {
         Ksztalt kwadrat = new Kwadrat("Czarny", 2.22);
 
         kolo.wyswietlInformacje();
-        System.out.println("");
+        System.out.println("\n");
         System.out.println(" pole powierzchni kola: " + kolo.obliczPolePowierzchni());
 
-        System.out.println("");
-        System.out.println("");
+        System.out.println("\n");
+        System.out.println("\n");
         kwadrat.wyswietlInformacje();
-        System.out.println("");
+        System.out.println("\n");
         System.out.println("pole powierzchni kwadrat:"  + kwadrat.obliczPolePowierzchni());
 
 
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("\n");
+        System.out.println("\n");
+
 
         Ksztalt kolo1 = new Kolo("Zelony" , 2.0);
         Ksztalt kwadrat1 = new Kwadrat("Niebieski", 2.22);
 
         kolo.wyswietlInformacje();
-        System.out.println("");
+        System.out.println("\n");
         System.out.println(" pole powierzchni kola: " + kolo1.obliczPolePowierzchni());
 
-        System.out.println("");
-        System.out.println("");
+        System.out.println("\n");
+        System.out.println("\n");
         kwadrat.wyswietlInformacje();
-        System.out.println("");
+        System.out.println("\n");
         System.out.println("pole powierzchni kwadrat:"  + kwadrat1.obliczPolePowierzchni());
 
 
